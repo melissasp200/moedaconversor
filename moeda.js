d273aaa6828640069c00ae207d5f2a36ca24eaf3
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-10-24 09:58:36 -0300

    ADD USD EUR BRL

--- moeda.js
+++ moeda.js
@@ -5,6 +5,14 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
 
+    // Definir taxa de câmbio fixa com utilização de objeto
+    const exchangeRates = {
+
+        USD: {EUR:XX, BRL:XX},
+
+    
+
+    };
 
 
 })
