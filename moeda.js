6078c62ae0454d271db5ae565f7ff42cf5b67a5b
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-10-24 09:53:30 -0300

    first commit

---
+++ moeda.js
@@ -0,0 +1,10 @@
+document.getElementById('currency-form').addEventListener('submit', function(event){
+
+    // Obter valores de entrada do formulário
+    const valor = parseFloat (document.getElementById('Valor').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+
+
+})
